051b335ea1bedb0738235533a96790583c09a68e
dongsen2019 <378250523@qq.com>
2020-04-25 16:16:17 +0800

    2019/11/13 by dongsen

---
+++ python_87-shutil.py
@@ -0,0 +1,30 @@
+# 高级模块  shutil
+
+import shutil
+
+# copy 复制文件 把一个文件拷贝到指定的目录中
+"""
+参数1：要拷贝的文件的路径名
+参数2：需要拷贝到别处的路径名，可以重命名
+文件不存在则报错：FileNotFoundError: [Errno 2] No such file or directory: '.\\tes.py'
+"""
+# shutil.copy(".\\test.py", "..\\testagain.py")
+
+# copy2 和 copy方法一样，可以把文件拷贝到指定目录，保留了源文件的信息（操作时间和权限等）
+
+# copyfile 拷贝文件的内容（打开文件，读取内容，写入到新的文件中）
+"""
+将 src 文件的内容（不含元数据）拷贝到 dst 文件并返回 dst。 src 和 dst 是字符串形式的路径。 dst 必须是完整的目标文件名
+"""
+# shutil.copyfile(".\\test.py", "..\\testagain.py")  # 如果路径二的文件不存在则新建一个文件
+
+# copytree 可以把整个目录结构和文件全部拷贝到指定目录中，但是要求指定的目录文件夹目录必须不存在
+# 否则会报错  FileExistsError: [WinError 183] 当文件已存在时，无法创建该文件。: '../a'
+# shutil.copytree("../practice", "../a")
+
+# rmtree() 删除整个文件夹
+# shutil.rmtree("../a")
+
+# move 移动文件或文件夹到指定目录，也可以用于修改文件夹或文件的名称
+shutil.move("../a", "../practice")  # 移动
+shutil.move("../a", "../practice/abc") # 改名
---
+++ python_88-zipfile.py
@@ -0,0 +1,54 @@
+# 压缩模块 zipfile
+
+import zipfile
+
+"""
+zipfile.ZipFile(路径包名，模式，压缩方式)
+"""
+
+# with zipfile.ZipFile("..//spam.zip", 'w') as myzip:
+#     myzip.write("E:/pycharm/practice/python_1-10.py")
+#     myzip.write("E:/pycharm/practice/test.py")
+
+# 解压缩后的文件夹包的路径名为压缩时的路径名
+# with zipfile.ZipFile("../spam.zip", 'r') as myzip:
+#     myzip.extractall("../a")
+
+import os
+
+# 不同的压缩方式，压缩文件的大小差距很大
+"""
+zipfile.ZIP_STORED
+未被压缩的归档成员的数字常数。
+
+zipfile.ZIP_DEFLATED
+常用的 ZIP 压缩方法的数字常数。需要 zlib 模块。
+
+zipfile.ZIP_BZIP2
+BZIP2 压缩方法的数字常数。需要 bz2 模块。
+
+3.3 新版功能.
+
+zipfile.ZIP_LZMA
+LZMA 压缩方法的数字常数。需要 lzma 模块。
+"""
+# with zipfile.ZipFile("..//spam2.zip", 'w') as myzip:
+#     res = os.listdir("../practice")
+#     for i in res:
+#         myzip.write(i)
+
+with zipfile.ZipFile("..//spam2.zip", 'w', zipfile.ZIP_LZMA) as myzip:
+    res = os.listdir("../practice")
+    for i in res:
+        myzip.write(i)
+
+import  shutil
+
+# 使用 shutil.make_archive() 这个方法压缩文件比zip更方便
+"""
+参数1 创建压缩文件名称
+参数2 指定的压缩格式 zip or tar
+参数3 要压缩的文件或文件路径
+"""
+
+shutil.make_archive("../spam3", "zip", "../practice")
